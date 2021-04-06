632fe0cd9e499f3db39830aecd041b244c4a1586
Carlos-Castillo-Garcia <73341536+Carlos-Castillo-Garcia@users.noreply.github.com>
2021-04-06 19:57:02 +0200

    prototipo proyecto con varias mentanas

---
+++ src/main/java/com/mycompany/proyprot/GastosController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyprot;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author CARLOS
+ */
+public class GastosController{
+    @FXML
+    private void backgasto() throws IOException{
+        App.setRoot("menu");
+    }
+}
--- src/main/java/com/mycompany/proyprot/MenuController.java
+++ src/main/java/com/mycompany/proyprot/MenuController.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.proyprot;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -13,7 +14,26 @@ public class MenuController extends LoginController{
     @FXML
     private Label usuariologeado;
     
+    @FXML
     private void prueba(){
         usuariologeado.setText("EL ususario");
+    }
+    
+    @FXML
+    private void atras() throws IOException{
+        App.setRoot("login");
+    }
+    @FXML
+    private void ingreso() throws IOException{
+        App.setRoot("ingreso");
+    }
+    
+    @FXML
+    private void gasto() throws IOException{
+        App.setRoot("gastos");
+    }
+    @FXML
+    private void newpiso() throws IOException{
+        App.setRoot("newpiso");
     }
 }
