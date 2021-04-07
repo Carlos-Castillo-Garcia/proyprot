78811d0f3e1b3b0b01bb57543fb2cb0e4966f9fc
Usuario <Usuario@LAPTOP-B5DJ2DJK>
2021-04-07 16:59:18 +0200

    ventanas creadas

---
+++ src/main/java/com/mycompany/proyprot/MeterpisoController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyprot;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Usuario
+ */
+public class MeterpisoController implements Initializable {   
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
