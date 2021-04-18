b0c1076bd6100011c45356352e7f09c156786a96
Carlos-Castillo-Garcia <73341536+Carlos-Castillo-Garcia@users.noreply.github.com>
2021-04-18 21:44:20 +0200

    empezado el registro de usuarios

---
+++ src/main/java/com/mycompany/DAO/UserDAO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.DAO;
+
+import com.mycompany.models.Users;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author CARLOS
+ */
+public class UserDAO {
+    private Connection conexion;
+    
+    public void conectar() throws ClassNotFoundException, SQLException, IOException {
+        String host = "LocalHost";
+        String port = "3306";
+        String dbname = "casa_expres";
+        String username = "root";
+        String password = "123456";
+        
+        conexion = DriverManager.getConnection("jdbc:mariadb://"+
+                                                host + ":" + port + "/" + dbname
+                + "?serverTimezone=UTC",username,password);
+        
+    }
+    
+    public void desconexion () throws SQLException{
+        conexion.close();
+    }
+    
+    public void insertar(Users user) throws SQLException, ClassNotFoundException, IOException{
+        String sql = "INSERT INTO casa_expres.user (username, password, email) VALUES (?,?,?)";
+        
+        PreparedStatement sentencia = conexion.prepareStatement(sql);
+        sentencia.setString(1, user.getNombre());
+        sentencia.setString(2, user.getContrasena());
+        sentencia.setString(3, user.getEmail());
+        sentencia.executeUpdate();
+    }
+    
+//    public void log(Users user) throws SQLException, ClassNotFoundException, IOException{
+//        String sql = "SELECT email, password FROM user WHERE email = ?";
+//        
+//        PreparedStatement sentencia = conexion.prepareStatement(sql);
+//        /*hacer login con un select filtrando por el email*/
+//    }
+}
