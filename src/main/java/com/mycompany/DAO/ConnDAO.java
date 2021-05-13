686ef68f8115af11584a6cc8632871a45852ff99
PORTATIL_2 <PORTATIL_2@DESKTOP-S09DCR1>
2021-05-13 21:44:31 +0200

    mod carpeta fin

--- src/main/java/com/mycompany/DAO/ConnDAO.java
+++ src/main/java/com/mycompany/DAO/ConnDAO.java
@@ -5,10 +5,14 @@
  */
 package com.mycompany.DAO;
 
+import com.mycompany.proyprot.App;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  *
@@ -26,31 +30,31 @@ public class ConnDAO {
     public static Connection conectar() throws ClassNotFoundException, SQLException, IOException {
         
         Connection conexion;
-        String host = "LocalHost";
-        String port = "3306";
-        String dbname = "casa_expres";
-        String username = "root";
-        String password = 
-                "123456"
-//                  "Rowl3t1005"
-                ;
-        
-        conexion = DriverManager.getConnection("jdbc:mariadb://"+
-                                                host + ":" + port + "/" + dbname
-                + "?serverTimezone=UTC",username,password);
-        
-        return conexion;
-//        Properties configuration = new Properties();
-//        configuration.load(new FileInputStream(new File(App.class.getResource("connectionDB.properties").getPath())));
-//        String host = configuration.getProperty("host");
-//        String port = configuration.getProperty("port");
-//        String name = configuration.getProperty("name");
-//        String username = configuration.getProperty("username");
-//        String password = configuration.getProperty("password");
-//
-//        conexion = DriverManager.getConnection("jdbc:mariadb://" + host + ":" + port + "/" + name + "?serverTimezone=UTC",
-//                username, password);
+//        String host = "LocalHost";
+//        String port = "3306";
+//        String dbname = "casa_expres";
+//        String username = "root";
+//        String password = 
+//                "123456"
+////                  "Rowl3t1005"
+//                ;
+//        
+//        conexion = DriverManager.getConnection("jdbc:mariadb://"+
+//                                                host + ":" + port + "/" + dbname
+//                + "?serverTimezone=UTC",username,password);
+//        
 //        return conexion;
+        Properties configuration = new Properties();
+        configuration.load(new FileInputStream(new File(App.class.getResource("connectionDB.properties").getPath())));
+        String host = configuration.getProperty("host");
+        String port = configuration.getProperty("port");
+        String name = configuration.getProperty("name");
+        String username = configuration.getProperty("username");
+        String password = configuration.getProperty("password");
+
+        conexion = DriverManager.getConnection("jdbc:mariadb://" + host + ":" + port + "/" + name + "?serverTimezone=UTC",
+                username, password);
+        return conexion;
     }
     
     /**
